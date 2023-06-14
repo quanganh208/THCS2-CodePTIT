8f947f6f88dd7211d2a5938b68adc8c87293c3bb
quanganh.208 <nquanganh478@gmail.com>
2023-06-14 15:54:06 +0700

    Code ngu xin đừng chửi :<

--- C06024 - TỔNG HAI SỐ NGUYÊN LỚN.cpp
+++ C06024 - TỔNG HAI SỐ NGUYÊN LỚN.cpp
@@ -1,40 +1,52 @@
 #include <stdio.h>
 #include <string.h>
-
-int main() {
-    int t; scanf("%d\n", &t);
-    while (t--) {
-        char a[1000], b[1000], a0[1000] = "", b0[1000] = "";
+void strRev(char a[1000])
+{
+    char b[1000];
+    strcpy(b, a);
+    for (int i = 0; i < strlen(a); i++)
+        a[i] = b[strlen(b) - 1 - i];
+}
+int main()
+{
+    int t;
+    scanf("%d\n", &t);
+    while (t--)
+    {
+        char a[1000], b[1000];
         gets(a);
         scanf("\n");
         gets(b);
 
         // Đảo ngược chuỗi
-        for (int i = strlen(a)-1; i >= 0; i--) {
-			a0[strlen(a)-1-i] = a[i];
-		}
-		for (int i = strlen(b)-1; i >= 0; i--) {
-			b0[strlen(b)-1-i] = b[i];
-		}
-
-        if (strlen(a0) >= strlen(b0)) {
-            strcat(a0, "0");
-            while(strlen(b0) < strlen(a0)) strcat(b0, "0");
+        strRev(a);
+        strRev(b);
+        // Bù 0
+        if (strlen(a) >= strlen(b))
+        {
+            strcat(a, "0");
+            while (strlen(b) < strlen(a))
+                strcat(b, "0");
         }
-        else {
-            strcat(b0, "0");
-            while(strlen(a0) < strlen(b0)) strcat(a0, "0");
+        else
+        {
+            strcat(b, "0");
+            while (strlen(a) < strlen(b))
+                strcat(a, "0");
         }
 
         // Tính toán
         int s[10000] = {0}, remainder = 0;
-        for (int i = 0; i < strlen(a0); i++) {
-            int x = (a0[i] - '0') + (b0[i] - '0') + remainder;
+        for (int i = 0; i < strlen(a); i++)
+        {
+            int x = (a[i] - '0') + (b[i] - '0') + remainder;
             remainder = x / 10;
             s[i] = x % 10;
         }
-        for (int i = strlen(a0) - 1; i >= 0; i--) {
-            if (i == strlen(a0) - 1 && s[i] == 0) continue;
+        for (int i = strlen(a) - 1; i >= 0; i--)
+        {
+            if (i == strlen(a) - 1 && s[i] == 0)
+                continue;
             printf("%d", s[i]);
         }
 
